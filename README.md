053910c41ccb5a3845002132a42039c5747fb069
Teodor G <teo@zeoflow.com>
2021-04-10 23:24:31 +0300

    updated User's class documentation

--- README.md
+++ README.md
@@ -88,6 +88,136 @@ Memo.init(context)
   .build();
 ```
 
+### 3. Injector
+#### 3.1 Build MemoEntity Class
+```java
+/**
+ * the entity generated will be named UserProfile_MemoEntity
+ * it was annotated with @MemoEntity("UserProfile")
+ *
+ * the entity will be encrypted using the "G15y3aV9M8d" key
+ * it was annotated with @EncryptEntity("G15y3aV9M8d")
+ */
+@MemoEntity("UserProfile")
+@EncryptEntity("G15y3aV9M8d")
+public class User
+{
+
+    /**
+     * the default value will be "zeoflow"
+     *
+     * generated field will be named username
+     *
+     * this field is observable
+     * it was annotated with @Observable
+     */
+    @KeyName("username")
+    @Observable
+    protected final String userUsername = "zeoflow";
+
+    /**
+     * generated field name will be login - lowerCamel
+     *
+     * this field will have its own onChangedListener
+     * it was annotated with @Listener
+     */
+    @Listener
+    protected final boolean login = false;
+
+    /* the default value will be 1 */
+    @KeyName("views")
+    protected final int viewsCount = 1;
+
+    /* the default value will be null */
+    @KeyName("userinfo")
+    protected PrivateInfo privateInfo;
+
+    /**
+     * preference putter function for userUsername.
+     *
+     * @param userUsername function in
+     *
+     * @return function out
+     */
+    @MemoFunction("username")
+    public String putUserUsernameFunction(String userUsername)
+    {
+        return "Hello, " + userUsername;
+    }
+
+    /**
+     * preference getter function for userUsername.
+     *
+     * @param userUsername function in
+     *
+     * @return function out
+     */
+    @MemoFunction("username")
+    public String getUserUsernameFunction(String userUsername)
+    {
+        return userUsername + "!!!";
+    }
+
+    /**
+     * preference putter function example for visitCount's auto increment.
+     *
+     * @param count function in
+     *
+     * @return function out
+     */
+    @MemoFunction("views")
+    public int putVisitCountFunction(int count)
+    {
+        return ++count;
+    }
+
+    /**
+     * preference getter compound function for following fields.
+     *
+     * Params declared inside @MemoCompoundFunction's annotation
+     * @param username function in
+     * @param views function in
+     *
+     * @return $username $views
+     */
+    @MemoCompoundFunction(values = {"username", "views"})
+    public String getUserViews(String username, int views)
+    {
+        return username + " " + views;
+    }
+
+    /**
+     * preference getter compound function for following fields.
+     *
+     * Params declared inside @MemoCompoundFunction's annotation
+     * @param userinfo function in
+     *
+     * @return $first_name $last_name
+     */
+    @MemoCompoundFunction(values = {"userinfo"})
+    public String getFullName(PrivateInfo userinfo)
+    {
+        return userinfo.getFirstName() + " " + userinfo.getLastName();
+    }
+
+    /**
+     * preference getter compound function for following fields.
+     *
+     * Params declared inside @MemoCompoundFunction's annotation
+     * @param userinfo function in
+     * @param views function in
+     *
+     * @return $first_name $last_name, views count $views
+     */
+    @MemoCompoundFunction(values = {"userinfo", "views"})
+    public String getFullNameAndViews(PrivateInfo userinfo, int views)
+    {
+        return userinfo.getFirstName() + " " + userinfo.getLastName() + ", views count: " + views;
+    }
+
+}
+```
+
 ## License
     Copyright (C) 2021 ZeoFlow S.R.L.
     
--- demo/src/main/java/com/zeoflow/demo/entities/User.java
+++ demo/src/main/java/com/zeoflow/demo/entities/User.java
@@ -8,55 +8,75 @@ import com.zeoflow.memo.annotation.MemoEntity;
 import com.zeoflow.memo.annotation.MemoFunction;
 import com.zeoflow.memo.annotation.Observable;
 
+/**
+ * the entity generated will be named UserProfile_MemoEntity
+ * it was annotated with @MemoEntity("UserProfile")
+ *
+ * the entity will be encrypted using the "G15y3aV9M8d" key
+ * it was annotated with @EncryptEntity("G15y3aV9M8d")
+ */
 @MemoEntity("UserProfile")
-@EncryptEntity("G15y3aV9M8dHbmV4vC9EZmDxRgAoWd")
+@EncryptEntity("G15y3aV9M8d")
 public class User
 {
 
+    /**
+     * the default value will be "zeoflow"
+     *
+     * generated field will be named username
+     *
+     * this field is observable
+     * it was annotated with @Observable
+     */
     @KeyName("username")
     @Observable
-    protected final String userNickName = "zeoflow";
+    protected final String userUsername = "zeoflow";
 
     /**
-     * key value will be 'Login'. (login's camel uppercase)
+     * generated field name will be login - lowerCamel
+     *
+     * this field will have its own onChangedListener
+     * it was annotated with @Listener
      */
     @Listener
     protected final boolean login = false;
 
+    /* the default value will be 1 */
     @KeyName("views")
     protected final int viewsCount = 1;
 
+    /* the default value will be null */
     @KeyName("userinfo")
     protected PrivateInfo privateInfo;
 
     /**
-     * preference putter function about userNickName.
+     * preference putter function for userUsername.
      *
-     * @param nickname function in
-     *
-     * @return function out
-     */
-    @MemoFunction("username")
-    public String putUserNickFunction(String nickname)
-    {
-        return "Hello, " + nickname;
-    }
-
-    /**
-     * preference getter function about userNickName.
-     *
-     * @param nickname function in
+     * @param userUsername function in
      *
      * @return function out
      */
     @MemoFunction("username")
-    public String getUserNickFunction(String nickname)
+    public String putUserUsernameFunction(String userUsername)
     {
-        return nickname + "!!!";
+        return "Hello, " + userUsername;
     }
 
     /**
-     * preference putter function example about visitCount's auto increment.
+     * preference getter function for userUsername.
+     *
+     * @param userUsername function in
+     *
+     * @return function out
+     */
+    @MemoFunction("username")
+    public String getUserUsernameFunction(String userUsername)
+    {
+        return userUsername + "!!!";
+    }
+
+    /**
+     * preference putter function example for visitCount's auto increment.
      *
      * @param count function in
      *
@@ -68,18 +88,44 @@ public class User
         return ++count;
     }
 
+    /**
+     * preference getter compound function for following fields.
+     *
+     * Params declared inside @MemoCompoundFunction's annotation
+     * @param username function in
+     * @param views function in
+     *
+     * @return $username $views
+     */
     @MemoCompoundFunction(values = {"username", "views"})
-    public String getUserFullName(String username, int views)
+    public String getUserViews(String username, int views)
     {
         return username + " " + views;
     }
 
+    /**
+     * preference getter compound function for following fields.
+     *
+     * Params declared inside @MemoCompoundFunction's annotation
+     * @param userinfo function in
+     *
+     * @return $first_name $last_name
+     */
     @MemoCompoundFunction(values = {"userinfo"})
     public String getFullName(PrivateInfo userinfo)
     {
         return userinfo.getFirstName() + " " + userinfo.getLastName();
     }
 
+    /**
+     * preference getter compound function for following fields.
+     *
+     * Params declared inside @MemoCompoundFunction's annotation
+     * @param userinfo function in
+     * @param views function in
+     *
+     * @return $first_name $last_name, views count $views
+     */
     @MemoCompoundFunction(values = {"userinfo", "views"})
     public String getFullNameAndViews(PrivateInfo userinfo, int views)
     {
