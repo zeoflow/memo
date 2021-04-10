7a911c6c608c0ef7e28c8bffb0776c090b1bd843
Teodor G <teo@zeoflow.com>
2021-04-10 18:20:21 +0300

    code cleaned-up
    private methods deleted

--- memo-compiler/src/main/java/com/zeoflow/memo/processor/PreferenceFieldMethodGenerator.java
+++ memo-compiler/src/main/java/com/zeoflow/memo/processor/PreferenceFieldMethodGenerator.java
@@ -23,7 +23,6 @@ import com.zeoflow.jx.file.MethodSpec;
 import com.zeoflow.jx.file.ParameterSpec;
 import com.zeoflow.jx.file.ParameterizedTypeName;
 import com.zeoflow.jx.file.TypeName;
-import com.zeoflow.memo.annotation.AESEncryption;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,9 +39,6 @@ public class PreferenceFieldMethodGenerator
     private static final String KEYNAME_POSTFIX = "KeyName";
     private static final String HAS_PREFIX = "contains";
     private static final String REMOVE_PREFIX = "remove";
-    private static final String INSTANCE_CONVERTER = "value";
-    private static final String EDIT_METHOD = "edit()";
-    private static final String APPLY_METHOD = "apply()";
     private final PreferenceKeyField keyField;
     private final PreferenceEntityAnnotatedClass annotatedEntityClazz;
     private final String preference = "Memo";
@@ -144,94 +140,6 @@ public class PreferenceFieldMethodGenerator
         return keyField.keyName + "Observable.setValue(" + keyField.keyName.toLowerCase() + ")";
     }
 
-    private MethodSpec generateObjectGetter()
-    {
-        ClassName converterClazz = ClassName.get(keyField.converterPackage, keyField.converter);
-        String typeName = keyField.typeName.box().toString();
-        if (typeName.contains("<")) typeName = typeName.substring(0, typeName.indexOf("<"));
-        MethodSpec.Builder builder =
-                MethodSpec.methodBuilder(getGetterPrefixName())
-                        .addModifiers(PUBLIC)
-                        .addAnnotation(Nullable.class)
-                        .addStatement(
-                                "$T $N = new $T($N.class)",
-                                converterClazz,
-                                INSTANCE_CONVERTER,
-                                converterClazz,
-                                typeName);
-        if (isEncryption())
-        {
-            builder.addStatement(
-                    "return ($T)" + getObjectEncryptedGetterStatement(),
-                    keyField.typeName.box(),
-                    INSTANCE_CONVERTER,
-                    AESEncryption.class,
-                    preference,
-                    keyField.keyName,
-                    keyField.value,
-                    keyField.value,
-                    getEncryptionKey());
-        } else
-        {
-            builder.addStatement(
-                    "return ($T)" + getObjectGetterStatement(),
-                    keyField.typeName.box(),
-                    INSTANCE_CONVERTER,
-                    preference,
-                    keyField.keyName,
-                    keyField.value);
-        }
-        builder.returns(keyField.typeName);
-        return builder.build();
-    }
-
-    private MethodSpec generateObjectSetter()
-    {
-        ClassName converterClazz = ClassName.get(keyField.converterPackage, keyField.converter);
-        String typeName = keyField.typeName.box().toString();
-        if (typeName.contains("<")) typeName = typeName.substring(0, typeName.indexOf("<"));
-        MethodSpec.Builder builder =
-                MethodSpec.methodBuilder(getSetterPrefixName())
-                        .addModifiers(PUBLIC)
-                        .addParameter(keyField.typeName, keyField.keyName.toLowerCase())
-                        .addStatement(
-                                "$T $N = new $T($N.class)",
-                                converterClazz,
-                                INSTANCE_CONVERTER,
-                                converterClazz,
-                                typeName);
-        if (isEncryption())
-        {
-            builder.addStatement(
-                    getSetterEncryptStatement(),
-                    preference,
-                    EDIT_METHOD,
-                    keyField.keyName,
-                    AESEncryption.class,
-                    INSTANCE_CONVERTER + ".convertObject(" + keyField.keyName.toLowerCase() + ")",
-                    getEncryptionKey(),
-                    APPLY_METHOD);
-        } else
-        {
-            builder.addStatement(
-                    getSetterStatement(),
-                    preference,
-                    EDIT_METHOD,
-                    keyField.keyName,
-                    INSTANCE_CONVERTER + ".convertObject(" + keyField.keyName.toLowerCase() + ")",
-                    APPLY_METHOD);
-        }
-        if (keyField.isObservable)
-        {
-            builder.addStatement(getOnLiveDataStatement());
-        }
-        if (keyField.isListener)
-        {
-            builder.addStatement(getOnChangedStatement());
-        }
-        return builder.build();
-    }
-
     private MethodSpec generateObjectKeyNameSpec()
     {
         return MethodSpec.methodBuilder(getKeyNamePostfixName())
@@ -284,28 +192,6 @@ public class PreferenceFieldMethodGenerator
         return REMOVE_PREFIX + StringUtils.toUpperCamel(this.keyField.keyName);
     }
 
-    private String getGetterTypeMethodName()
-    {
-        if (isEncryption())
-        {
-            return GETTER_PREFIX + StringUtils.toUpperCamel("String");
-        } else
-        {
-            return GETTER_PREFIX + StringUtils.toUpperCamel(this.keyField.typeStringName);
-        }
-    }
-
-    private String getSetterTypeMethodName()
-    {
-        if (isEncryption())
-        {
-            return SETTER_PREFIX + StringUtils.toUpperCamel("String");
-        } else
-        {
-            return SETTER_PREFIX + StringUtils.toUpperCamel(this.keyField.typeStringName);
-        }
-    }
-
     private String getGetterStatement()
     {
         if (annotatedEntityClazz.getterFunctionsList.containsKey(keyField.keyName))
@@ -341,65 +227,6 @@ public class PreferenceFieldMethodGenerator
         }
     }
 
-    private String getEncryptedGetterStatement()
-    {
-        if (annotatedEntityClazz.getterFunctionsList.containsKey(keyField.keyName))
-        {
-            String superMethodName =
-                    annotatedEntityClazz.getterFunctionsList.get(keyField.keyName).getSimpleName().toString();
-            return wrapperClassFormatting(
-                    String.format("super.%s(\n$T.decrypt(\n$N.getString($S, $S), $S, $S))", superMethodName));
-        }
-        return wrapperClassFormatting("$T.decrypt(\n$N.getString($S, $S), $S, $S)");
-    }
-
-    private String getObjectGetterStatement()
-    {
-        if (annotatedEntityClazz.getterFunctionsList.containsKey(keyField.keyName))
-        {
-            String superMethodName =
-                    annotatedEntityClazz.getterFunctionsList.get(keyField.keyName).getSimpleName().toString();
-            if (keyField.value instanceof String)
-            {
-                return String.format("super.%s(\n$N.convertType($N.getString($S, $S)))", superMethodName);
-            } else if (keyField.value instanceof Float)
-            {
-                return String.format(
-                        "super.%s(\n$N.convertType($N." + getGetterTypeMethodName() + "($S, $Lf)))",
-                        superMethodName);
-            } else
-            {
-                return String.format(
-                        "super.%s(\n$N.convertType($N." + getGetterTypeMethodName() + "($S, $L))",
-                        superMethodName);
-            }
-        } else
-        {
-            if (keyField.value instanceof String)
-            {
-                return "$N.convertType($N.getString($S, $S))";
-            } else if (keyField.value instanceof Float)
-            {
-                return "$N.convertType(\n$N." + getGetterTypeMethodName() + "($S, $Lf))";
-            } else
-            {
-                return "$N.convertType(\n$N." + getGetterTypeMethodName() + "($S, $L))";
-            }
-        }
-    }
-
-    private String getObjectEncryptedGetterStatement()
-    {
-        if (annotatedEntityClazz.getterFunctionsList.containsKey(keyField.keyName))
-        {
-            String superMethodName =
-                    annotatedEntityClazz.getterFunctionsList.get(keyField.keyName).getSimpleName().toString();
-            return String.format(
-                    "super.%s(\n$N.convertType($T.decrypt($N.getString($S, $S), $S, $S)))", superMethodName);
-        }
-        return "$N.convertType(\n$T.decrypt($N.getString($S, $S), $S, $S))";
-    }
-
     private String getSetterStatement()
     {
         if (annotatedEntityClazz.setterFunctionsList.containsKey(keyField.keyName))
@@ -410,21 +237,6 @@ public class PreferenceFieldMethodGenerator
         } else
         {
             return "Memo.put($S, $N)";
-        }
-    }
-
-    private String getSetterEncryptStatement()
-    {
-        if (annotatedEntityClazz.setterFunctionsList.containsKey(keyField.keyName))
-        {
-            return String.format(
-                    "$N.$N."
-                            + getSetterTypeMethodName()
-                            + "($S, $T.encrypt(\nString.valueOf(super.%s($N)), $S)).$N",
-                    annotatedEntityClazz.setterFunctionsList.get(keyField.keyName).getSimpleName());
-        } else
-        {
-            return "$N.$N." + getSetterTypeMethodName() + "($S, $T.encrypt(\nString.valueOf($N), $S)).$N";
         }
     }
 
@@ -446,31 +258,6 @@ public class PreferenceFieldMethodGenerator
                 + "("
                 + keyField.keyName.toLowerCase()
                 + ")";
-    }
-
-    private boolean isEncryption()
-    {
-        return annotatedEntityClazz.isEncryption;
-    }
-
-    private String getEncryptionKey()
-    {
-        return annotatedEntityClazz.encryptionKey;
-    }
-
-    private String wrapperClassFormatting(String statement)
-    {
-        if (keyField.value instanceof Boolean)
-        {
-            return String.format("Boolean.valueOf(%s).booleanValue()", statement);
-        } else if (keyField.value instanceof Integer)
-        {
-            return String.format("Integer.valueOf(%s).intValue()", statement);
-        } else if (keyField.value instanceof Float)
-        {
-            return String.format("Float.valueOf(%s).floatValue()", statement);
-        }
-        return statement;
     }
 
     private String getObserverName()
